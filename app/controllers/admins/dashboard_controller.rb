2756b667f543aabb5059ee44d51bfb070a2d9d14
usamay2u <usamasharif92@gmail.com>
2019-11-12 05:11:40 +0500

    added dashboard

--- app/controllers/admins/dashboard_controller.rb
+++ app/controllers/admins/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admins::DashboardController < ::ApplicationController
   before_action :authenticate_admin!
 
   def index
-
+    @users_count       = User.count
+    @events_count      = Event.count
+    @conferences_count = Conference.count
   end
 end
--- config/routes/admins.rb
+++ config/routes/admins.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
 
   devise_for :admins
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
 end
