5a7d6e9547a48a08da8f798afcd7ae3aa54376b0
usamay2u <usamasharif92@gmail.com>
2019-11-14 02:41:35 +0500

    configue active storage

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -2,6 +2,7 @@ class Conference < ApplicationRecord
   belongs_to :event
   has_many :conference_users
   has_many :users, through: :conference_users
+  has_one_attached :cover_photo
 
   validates :venue, :date, :start_time, :end_time, presence: true
 end
